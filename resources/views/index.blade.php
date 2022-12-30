b810ef2ef2920e889c4758366c86713bfa71b390
David VichethVan <121614028+David0010J@users.noreply.github.com>
2022-12-30 14:55:13 +0700

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,7 @@
 
 	<title>AdminKit Demo - Bootstrap 5 Admin Template</title>
 
-	<link href="/adminkit/static/css/app.css" rel="stylesheet">
+	<link href="/adminkit-3.1.0/static/css/app.css" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet">
 </head>
 
