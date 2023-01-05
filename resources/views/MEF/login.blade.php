bb33095ac5423c6ead595d2fd81a7fec3dd0ed66
David0010J <vicheth.vandavid96@gmail.com>
2023-01-05 14:38:44 +0700

    adding

---
+++ resources/views/MEF/login.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="MEF.css/style.css">
+</head>
+<body>
+    <section class="nav-container">
+        <aside class="logo"></aside>
+        <aside class="menu">
+            <a href="#">Login</a> | <a href="#">Register</a>
+        </aside>
+        <div>
+            <form>
+
+            </form>
+        </div>
+    </section>
+</body>
+</html>
